b1226b7d9224fa61d1ec8f9b0989e11cce015260
olikant <oliver_plauborg@hotmail.com>
2023-09-05 11:39:05 +0200

    plants
    s

---
+++ farming/Assets/Scripts/PlantLogic.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantLogic : MonoBehaviour
+{
+    float growth;
+    [SerializeField] float growthRate;
+    float water;
+    [SerializeField] float waterConsumptionRate;
+    
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        growth -= growthRate;
+        water -= waterConsumptionRate;
+       
+    }
+    private void OnTriggerStay2D(Collision collision)
+    {
+        Debug.Log("colide");
+        if (collision.gameObject.CompareTag("WaterBucket"))
+        {
+            water += 0.1f;
+            Debug.Log("water");
+        }
+    }
+
+}
