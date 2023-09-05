9270ef9782904c1605f8ab03f55013ff028a8d49
Maja-Box <100191460+Maja-Box@users.noreply.github.com>
2023-09-05 10:49:28 +0200

    tilestart

---
+++ farming/Assets/Scripts/GridManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width, height;
+    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Transform Cam;
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
+        }
+
+        Cam.transform.position = new Vector3((float)width/2 -0.5f,(float)height/2 - 0.5f, -10);
+    }
+
+    private void Start()
+    {
+        GenerateGrid();
+    }
+}
